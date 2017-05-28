04044dca88a96e83afdd476c25f86699646e0689
TKT_SS9_43 <juanman234@gmail.com>
2017-05-29 06:16:13 +1000

    dialog fullscreen frag

---
+++ app/src/main/java/info/androidhive/materialtabs/fragments/ModalFragment.java
@@ -0,0 +1,54 @@
+package info.androidhive.materialtabs.fragments;
+
+import android.app.Dialog;
+import android.app.DialogFragment;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.Window;
+import android.widget.RelativeLayout;
+
+import info.androidhive.materialtabs.R;
+
+/**
+ * Created by NEO on 29/5/17.
+ */
+public class ModalFragment extends DialogFragment {
+
+    public ModalFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_modal, container, false);
+    }
+
+    @Override
+    public Dialog onCreateDialog(final Bundle savedInstanceState) {
+
+        // the content
+        final RelativeLayout root = new RelativeLayout(getActivity());
+        root.setLayoutParams(new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT));
+
+        // creating the fullscreen dialog
+        final Dialog dialog = new Dialog(getActivity());
+        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        dialog.setContentView(root);
+        dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.BLUE));
+        dialog.getWindow().setLayout(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT);
+
+        return dialog;
+    }
+
+}
--- app/src/main/java/info/androidhive/materialtabs/fragments/OneFragment.java
+++ app/src/main/java/info/androidhive/materialtabs/fragments/OneFragment.java
@@ -1,15 +1,25 @@
 package info.androidhive.materialtabs.fragments;
 
+import android.app.DialogFragment;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import info.androidhive.materialtabs.R;
 
 
-public class OneFragment extends Fragment{
+public class OneFragment extends Fragment implements View.OnClickListener
+{
+
+    Button button;
+
+//    Button b = (Button)findVi
 
     public OneFragment() {
         // Required empty public constructor
@@ -18,13 +28,67 @@ public class OneFragment extends Fragment{
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+//        button = (Button) getView().findViewById(R.id.button);
+
+//        button.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Log.d("mako", "shaba");
+//            }
+//        });
+
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_one, container, false);
+
+
+
+        View view = inflater.inflate(R.layout.fragment_one, container, false);
+        button = (Button) view.findViewById(R.id.button);
+        button.setOnClickListener(this);
+        return view;
+
+
+
+//        return inflater.inflate(R.layout.fragment_one, container, false);
+
     }
 
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.button:
+
+
+                ModalFragment modal = new ModalFragment();
+
+                Log.d("mako", "tkt : "+modal);
+
+//                FragmentManager manager = getChildFragmentManager();
+
+
+//                DialogFragment dialogFragment = new DialogFragment ();
+                modal.show(getActivity().getFragmentManager(),"simple dialog");
+
+//                seven.show(manager, "tag");
+
+//                ExampleDialogFragment dialog = new ExampleDialogFragment ();
+//                seven .show(getFragmentManager(), "example");
+
+//                FragmentTransaction transaction = manager.beginTransaction();
+//                transaction.replace(R.id.viewpager, seven);
+//                transaction.commit();
+
+
+
+
+
+                break;
+
+        }
+    }
 }
